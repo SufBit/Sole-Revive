72f8c246b60977c909ac946f33410be421d5d58b
Sufyan Arshad <sufyanarshad@my.unt.edu>
2023-06-21 19:45:05 -0500

    changed some text

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -16,16 +16,16 @@ const NavigationBar = () => {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
           <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
+          <Nav.Link href="#link">Buy</Nav.Link>
           <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1">Sell</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.2">
-              Another action
+              About Us
             </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3"></NavDropdown.Item>
             <NavDropdown.Divider />
             <NavDropdown.Item href="#action/3.4">
-              Separated link
+              Contact Us
             </NavDropdown.Item>
           </NavDropdown>
         </Nav>
